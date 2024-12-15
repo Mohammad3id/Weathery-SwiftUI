3f6fcc139cddf261955f79147332d8312bad7618
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2024-12-15 13:04:48 +0200

    Initial Commit

---
+++ Weathery/WeatheryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatheryApp.swift
+//  Weathery
+//
+//  Created by Mohammad Eid on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatheryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
